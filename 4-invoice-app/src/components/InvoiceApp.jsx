3462a37ce20b664ca78c38e5ec410b31bb155cad
Alexis San Martin Orellana <alexis.sanmartin@gmail.com>
2024-04-27 22:10:27 -0400

    avance tarea

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -11,25 +11,54 @@ export const InvoiceApp = () => {
 
     return (
         <>
-            <h1>Ejmplo Factura</h1>
-            <ul>
-                <li>Id: { id }</li>
-                <li>Name: { name }</li>
-            </ul>
-            <h3>Datos del Cliente</h3>
-            <ul>
-                <li>{ nameClient } { lastName }</li>
-                <li>{ country }</li>
-                <li>{ city }</li>
-                <li>{ street } { number }</li>
-            </ul>
-            <h3>Datos de la empresa</h3>
-            <ul>
-                <li>{ nameEmpresa }</li> 
-                <li>{ fiscalNumber }</li>   
-            </ul>  
-            <h4>Productos de la factura</h4>
+            <div className="container">
+                <div className="card my-3">
+                    <div className="card-header">
+                        Ejmplo Factura
+                    </div>
+                    <div className="card-body">
+                        <ul className="list-group">
+                            <li className="list-group-item">Id: { id }</li>
+                            <li className="list-group-item">Name: { name }</li>
+                        </ul>
+                        <div className="row my-3">
+                            <div className="col">
+                                <h3>Datos del Cliente</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{ nameClient } { lastName }</li>
+                                    <li className="list-group-item">{ country } / { city }</li>
+                                    <li className="list-group-item">{ street } { number }</li>
+                                </ul>
+                            </div>
+                            <div className="col">
+                                <h3>Datos de la empresa</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{ nameEmpresa }</li> 
+                                    <li className="list-group-item">{ fiscalNumber }</li>   
+                                </ul>
+                            </div>
+                        </div>  
+                        <h4>Productos de la factura</h4>
+                        <table className="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th align="left">Producto</th>
+                                    <th align="center">Precio</th>
+                                    <th align="right">Cantidad</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {items.map(({ id, product, price, quantity }) => (<tr key={ id }>
+                                        <td align="left">{ product }</td>
+                                        <td align="right">{ price }</td>
+                                        <td align="right">{ quantity }</td>
+                                    </tr>
+                                    ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
     )
-
 }

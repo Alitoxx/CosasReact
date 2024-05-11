b2b737b35c9fec337277a9f8edcd3bdc7fe43638
Alexis San Martin Orellana <alexis.sanmartin@gmail.com>
2024-05-11 11:10:52 -0400

    nueva aplicacion

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,72 @@
 export const CartApp = () => {
     return(
         <>
-            <h3>CartApp</h3>
+        
+            <div className="container">
+                <h3>CartApp</h3>
+                <div className="row">
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico RGB</h5>
+                                <p className="card-text">Teclado Mecanico con luces RGB Switch Blue</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Mouse Gamer</h5>
+                                <p className="card-text">Mouse Gamer Negro</p>
+                                <p className="card-text">$ 2000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>     
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Monitor Negro</h5>
+                                <p className="card-text">Monitor Negro 2K</p>
+                                <p className="card-text">$ 3000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>     
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico RGB</h5>
+                                <p className="card-text">Teclado Mecanico con luces RGB Switch Blue</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>     
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico RGB</h5>
+                                <p className="card-text">Teclado Mecanico con luces RGB Switch Blue</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>     
+                    <div className="col-4 my-2">
+                        <div className="card">                        
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico RGB</h5>
+                                <p className="card-text">Teclado Mecanico con luces RGB Switch Blue</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>     
+                        </div>     
+                    </div>          
+                </div>
+            </div>
         </>
     )
 }
